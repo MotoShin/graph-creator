877dcffe278d0ba2cc0629ef0f3e3d1ae5270d10
Moto Shinriki <mshinrik@yahoo-corp.jp>
2020-10-25 13:56:12 +0900

    add comment

--- functional/numpy2img.py
+++ functional/numpy2img.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 class NumpyToImg:
     def __init__(self, x_axis_name, y_axis_name, output_file_name, column_name, x, y_values):
+        """
+        constructor
+        """
         self.x_axis_name = x_axis_name
         self.y_axis_name = y_axis_name
         self.output_file_name = output_file_name
@@ -11,6 +14,9 @@ class NumpyToImg:
         self.y_values = y_values
 
     def create_img(self):
+        """
+        create image
+        """
         y_values_trans = self.y_values.T
         with plt.style.context(['science', 'no-latex']):
             fig, ax = plt.subplots(facecolor="w")
